54a940ffb148302eab381db997a45ed97504b9a8
Israel Aguilar <iaguilaror@gmail.com>
2017-08-30 11:42:03 -0500

    mkfile completed, config has variables for reference genome and chain file.
    mkfile still needs testing because no functional installation of Crossmap.py has been used.

---
+++ 001/config.mk
@@ -0,0 +1,3 @@
+###
+REFERENCE="/home/iaguilar/Ongoing_Projects/Crossmaps/References/1000Genomes/hs37d5.fa.gz"
+CHAIN="/home/iaguilar/Ongoing_Projects/Crossmaps/References/chains/GRCh37_to_GRCh38.chain.gz"
