ce40135bb1b4be5d238b6fd4449d59a38c8ad6cb
Israel Aguilar <iaguilaror@gmail.com>
2017-09-07 13:08:57 -0500

    First version for test

--- 001/config.mk
+++ 001/config.mk
@@ -1,3 +1,3 @@
 ###
-REFERENCE="/home/iaguilar/Ongoing_Projects/Crossmaps/References/1000Genomes/hs37d5.fa.gz"
-CHAIN="/home/iaguilar/Ongoing_Projects/Crossmaps/References/chains/GRCh37_to_GRCh38.chain.gz"
+REFERENCE="/home/iaguilar/Ongoing_Projects/mk-Crossmap/References/1000Genomes/hs37d5.fa.gz"
+CHAIN="/home/iaguilar/Ongoing_Projects/mk-Crossmap/References/chains/GRCh37_to_GRCh38.chain.gz"
