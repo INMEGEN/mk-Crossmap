96e01e1412ae0d2a0216d0b68c67a07382ccacaa
Israel Aguilar <iaguilaror@gmail.com>
2017-10-31 15:16:53 -0600

    beat module

--- 001/config.mk
+++ 001/config.mk
@@ -1,3 +1,6 @@
 ###
-REFERENCE="/home/iaguilar/Ongoing_Projects/mk-Crossmap/References/1000Genomes/hs37d5.fa.gz"
-CHAIN="/home/iaguilar/Ongoing_Projects/mk-Crossmap/References/chains/GRCh37_to_GRCh38.chain.gz"
+REFERENCE="/home/israel/Ongoing_projects/mk-Crossmap/reference/hs37d5.fa.gz"
+CHAIN="/home/israel/Ongoing_projects/mk-Crossmap/reference/GRCh37_to_GRCh38.chain.gz"
+
+###path to Crossmap.py
+CROSSMAP="/media/genomics/disco1/Programs/CrossMap-0.2.7/bin/CrossMap.py"
