7b9d124d7cd499defcbaa848b76fffc96d0afea3
Israel Aguilar <iaguilaror@gmail.com>
2017-11-02 11:39:16 -0600

    Code working, chain files still not crrect

--- 001/config.mk
+++ 001/config.mk
@@ -1,6 +1,10 @@
 ###
-REFERENCE="/home/israel/Ongoing_projects/mk-Crossmap/reference/hs37d5.fa.gz"
-CHAIN="/home/israel/Ongoing_projects/mk-Crossmap/reference/GRCh37_to_GRCh38.chain.gz"
+TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/hg19/hg19.fa"
+##TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/GRCh38/Homo_sapiens_assembly38.fasta"
+
+CHAIN="/media/genomics/disco2/Databases/Crossmap/GRCh37Tohg19.over.chain.gz"
+CHAIN="/media/genomics/disco2/Databases/Crossmap/hg38ToHg19.over.chain.gz"
 
 ###path to Crossmap.py
 CROSSMAP="/media/genomics/disco1/Programs/CrossMap-0.2.7/bin/CrossMap.py"
+BCFTOOLS="/media/genomics/disco1/Programs/bcftools_1.6/bcftools"
