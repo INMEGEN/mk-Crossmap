73178bf627ec7796fa495f5f48722916cf5b2cc4
Israel Aguilar <iaguilaror@gmail.com>
2017-11-02 23:52:25 -0600

    working beta with 100G test data, converting from hg28 to hg19

---
+++ analysis/001-vcf_conversion/config.mk
@@ -0,0 +1,16 @@
+###
+##TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/hg38/Homo_sapiens_assembly38.fasta"
+##TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa"
+##TARGET_REFERENCE="reference/reference_genomes/hg38/Homo_sapiens_assembly38.fasta"
+TARGET_REFERENCE="reference/reference_genomes/hg19/hg19.fa"
+
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/GRCh37Tohg19.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/hg38ToHg19.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/hg19ToHg38.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/GRCh37ToHg38.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/other_chainfiles/GRCh37_to_GRCh38.chain.gz"
+CHAIN="./reference/chainfiles/hg38ToHg19.over.chain.gz"
+
+###path to Crossmap.py
+CROSSMAP="/media/genomics/disco1/Programs/CrossMap-0.2.7/bin/CrossMap.py"
+BCFTOOLS="/media/genomics/disco1/Programs/bcftools_1.6/bcftools"
