7d3b7d3f1111bc8541792f5ef1f478485b829c6c
Israel Aguilar <iaguilaror@gmail.com>
2017-11-02 20:18:27 -0600

    tried diferent chain files

--- 001/config.mk
+++ 001/config.mk
@@ -1,9 +1,13 @@
 ###
-TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/hg19/hg19.fa"
-##TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/GRCh38/Homo_sapiens_assembly38.fasta"
+##TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/hg19/hg19.fa"
+##TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/hg38/Homo_sapiens_assembly38.fasta"
+TARGET_REFERENCE="/media/genomics/disco2/Databases/genome_references/GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa"
 
-CHAIN="/media/genomics/disco2/Databases/Crossmap/GRCh37Tohg19.over.chain.gz"
-CHAIN="/media/genomics/disco2/Databases/Crossmap/hg38ToHg19.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/GRCh37Tohg19.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/hg38ToHg19.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/hg19ToHg38.over.chain.gz"
+##CHAIN="/media/genomics/disco2/Databases/Crossmap/GRCh37ToHg38.over.chain.gz"
+CHAIN="/media/genomics/disco2/Databases/Crossmap/other_chainfiles/GRCh37_to_GRCh38.chain.gz"
 
 ###path to Crossmap.py
 CROSSMAP="/media/genomics/disco1/Programs/CrossMap-0.2.7/bin/CrossMap.py"
